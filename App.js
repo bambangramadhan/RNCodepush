a6cd5fe204cc1b0dab76e23eadf92f10e9f22e16
email@kotakmakan.id <bambang@kotakmakan.id>
2019-10-24 11:13:20 +0700

    Fix code push

--- App.js
+++ App.js
@@ -40,7 +40,7 @@ export class App extends React.PureComponent {
         </TouchableOpacity>
         <View>
           <Text>
-            {"Bismillah, kamu pasti berhasil, semangat"}
+            {"Take 4, action"}
           </Text>
           <Text style={{ marginTop: 150 }}>
             {JSON.stringify(this.state.logs)}
