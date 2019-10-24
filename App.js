c6907282e5df87c453ec85b3882cd14f89af8edf
email@kotakmakan.id <bambang@kotakmakan.id>
2019-10-24 11:30:40 +0700

    Fix version

--- App.js
+++ App.js
@@ -40,7 +40,7 @@ export class App extends React.PureComponent {
         </TouchableOpacity>
         <View>
           <Text>
-            {"Ayo bisa, berhasil kuy"}
+            {"Ayo bisa, berhasil kuy, kamu bisa"}
           </Text>
           <Text style={{ marginTop: 150 }}>
             {JSON.stringify(this.state.logs)}
